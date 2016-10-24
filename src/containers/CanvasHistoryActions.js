afad728ce4dc0ea53e5ac7b864493b49a1d92a74
OssamaZ <w4rz4zi@gmail.com>
2016-10-24 22:47:38 +0100

    Added tooltips for the canvas history actions

--- src/containers/CanvasHistoryActions.js
+++ src/containers/CanvasHistoryActions.js
@@ -1,13 +1,54 @@
 import React from 'react';
-import _ from 'lodash';
+import { connect } from 'react-redux';
 import classnames from 'classnames';
+import ReactTooltip from 'react-tooltip';
 
-const CanvasHistoryActions = () =>
+// Actions
+import {
+  clearCanvas
+} from '../actions/canvasActions';
+
+const CanvasHistoryActions = ({clearCanvas}) =>
   <div className='editor-actions'>
-    <a href="#" className='zmdi zmdi-delete' />
-    <a href="#" className={classnames('zmdi zmdi-undo', {'disabled': false})} />
-    <a href="#" className={classnames('zmdi zmdi-undo', {'disabled': true})} />
+    <ClearCanvasBtn clearCanvas={clearCanvas} />
+    <UndoBtn />
+    <RedoBtn />
     <span className=''>Changes Auto-saved</span>
   </div>
 
-export default CanvasHistoryActions;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    clearCanvas: () => dispatch(clearCanvas())
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CanvasHistoryActions);
+
+// Tiny buttons.. i LOVE functions
+const ClearCanvasBtn = ({clearCanvas}) =>
+  <a href="#" className='zmdi zmdi-delete' onClick={e => {
+      e.preventDefault();
+      clearCanvas();
+    }} data-tip data-for="clear-tooltip">
+    <ReactTooltip id='clear-tooltip' type="dark" effect="solid" place='bottom' class='tooltip'>
+      Clear canvas
+    </ReactTooltip>
+  </a>
+
+const UndoBtn = () =>
+  <a href="#" className={classnames('zmdi zmdi-undo', {'disabled': true})} onClick={e => {
+      e.preventDefault();
+    }} data-tip data-for="undo-tooltip">
+    <ReactTooltip id='undo-tooltip' type="dark" effect="solid" place='bottom' class='tooltip'>
+      Undo
+    </ReactTooltip>
+  </a>
+
+const RedoBtn = () =>
+  <a href="#" className={classnames('zmdi zmdi-redo', {'disabled': true})} onClick={e => {
+      e.preventDefault();
+    }} data-tip data-for="redo-tooltip">
+    <ReactTooltip id='redo-tooltip' type="dark" effect="solid" place='bottom' class='tooltip'>
+      Redo
+    </ReactTooltip>
+  </a>
